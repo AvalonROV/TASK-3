b8504ff6b35f2fd53282e67bb64aa9c6dbc2ff06
Ronak Sharma <ronaksharma@dyn027172.shef.ac.uk>
2018-04-02 20:33:07 +0100

    Final Version

--- Distortion Processing/Distortion.py
+++ Distortion Processing/Distortion.py
@@ -8,10 +8,8 @@ import glob
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-
-
-cbrow = 3 #https://stackoverflow.com/questions/31249037/calibrating-webcam-using-python-and-opencv-error?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-cbcol = 3
+cbrow = 5 #https://stackoverflow.com/questions/31249037/calibrating-webcam-using-python-and-opencv-error?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+cbcol = 5
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((cbrow * cbcol, 3), np.float32)
@@ -43,19 +41,30 @@ for fname in images:
         img = cv2.drawChessboardCorners(img, (cbrow,cbcol), corners2,ret)
         cv2.imshow('img',img)
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-        print(gray.shape[::-1])
+        #print(gray.shape[::-1])
+       # print(imgpoints)
 
-img = cv2.imread('12.jpg')
-h,  w = img.shape[:2]
+img = cv2.imread('test16.png')
+h,w = img.shape[:2]
 newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
 dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+cv2.imwrite('cal1.png',dst)
+#cv2.imshow('imgafe',dst)
+
+img = cv2.imread('cal1.png')
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+cv2.imwrite('cal2.png',dst)
+
+img = cv2.imread('cal2.png')
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+cv2.imwrite('cal3.png',dst)
+
 
 ## crop the image
 #x,y,w,h = roi
 #dst = dst[y:y+h, x:x+w]
-cv2.imwrite('calibresult_practice222.png',dst)
-cv2.imshow('imgafe',dst)
+
 
 cv2.waitKey(500)
 #cv2.destroyAllWindows()
